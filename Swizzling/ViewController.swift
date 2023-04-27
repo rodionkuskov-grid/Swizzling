5d5963f7e64457024c5dbe0713482d92a4836bc1
Rodion Kuskov <rkuskov@griddynamics.com>
2023-04-27 11:53:56 +0300

    1-st

--- Swizzling/ViewController.swift
+++ Swizzling/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let framework = SomeFramework()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        framework.printSun()
     }
-
-
 }
 
+
+// MARK: - Some framework
+
+class SomeFramework {
+    
+    init() {
+        swizzleFunctions()
+    }
+    
+    // MARK: - Public function which could be used outside the framework
+    @objc public dynamic func printSun() {
+        print("🌞")
+    }
+
+    // MARK: - Private function which will execute instead of printSun()
+    @objc private func printMoon() {
+        print("🌚")
+    }
+
+    private func swizzleFunctions() {
+        guard
+            let originalMethod = class_getInstanceMethod(SomeFramework.self, #selector(SomeFramework.printSun)),
+            let customMethod = class_getInstanceMethod(SomeFramework.self, #selector(SomeFramework.printMoon))
+        else { return }
+
+        method_exchangeImplementations(originalMethod, customMethod)
+    }
+}
