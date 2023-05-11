5f9c2c088f3a9f21b3d76f5fc28f31835887a6d9
Rodion Kuskov <rkuskov@griddynamics.com>
2023-05-11 12:29:45 +0300

    11-th

--- Swizzling/ViewController.swift
+++ Swizzling/ViewController.swift
@@ -8,56 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let foo = Foo()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        /* SomeFramework().printSun() */
-    }
-
-    @objc dynamic func printSunModified() {
-        print("🌞🌞")
+        foo.foo()
     }
 }
 
-extension SomeFramework {
-    static func swizzleFrameworkFunction() {
-        guard
-            let originalMethod = class_getInstanceMethod(SomeFramework.self, #selector(SomeFramework.printSun)),
-            let customMethod = class_getInstanceMethod(ViewController.self, #selector(ViewController.printSunModified))
-        else { return }
-
-        method_exchangeImplementations(originalMethod, customMethod)
+class Foo {
+    dynamic func foo() {
+        print("foo called")
     }
 }
 
-// MARK: - Какой-нибудь фреймворк
-
-class SomeFramework {
-    private static var isSwizzleCompleted: Bool = false
-
-    init() {
-        guard !SomeFramework.isSwizzleCompleted else { return }
-        swizzleFunctions()
-    }
-    
-    // MARK: - Открытая функция, которую могут использовать вне фреймворка
-    @objc public dynamic func printSun() {
-        print("🌞")
-    }
-
-    // MARK: - Закрытая функция, которая собирает аналитку и вызывает открытую функцию printSun()
-    /// В случае, если нужно вызывать оригинальную функцию тоже, необходимо добавить dynamic keyword.
-    @objc private dynamic func logAnalytics() {
-        print("Logging analytics.")
-        logAnalytics()
-    }
-
-    private func swizzleFunctions() {
-        guard
-            let originalMethod = class_getInstanceMethod(SomeFramework.self, #selector(SomeFramework.printSun)),
-            let customMethod = class_getInstanceMethod(SomeFramework.self, #selector(SomeFramework.logAnalytics))
-        else { return }
-
-        method_exchangeImplementations(originalMethod, customMethod)
-        SomeFramework.isSwizzleCompleted = true
+extension Foo {
+    @_dynamicReplacement(for: foo)
+    func fooReplacement() {
+        print("fooReplacement")
     }
 }
